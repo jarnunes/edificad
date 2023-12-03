3c76067151d56f126dc2b247cf4e0291b4cc85a4
Jardel Nunes <jarnunesc@gmail.com>
2023-12-03 09:28:46 -0300

    primeiro commit

---
+++ src/main/java/com/puc/edificad/EdificadApplication.java
@@ -0,0 +1,13 @@
+package com.puc.edificad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdificadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdificadApplication.class, args);
+    }
+
+}
