6261c6266289e2a86b502aef0ca37a165b2dd14c
Jardel Nunes <jarnunesc@gmail.com>
2023-12-04 11:47:28 -0300

    revisões visuais em css; js e campos obrigatórios

--- src/main/resources/static/js/elipse.js
+++ src/main/resources/static/js/elipse.js
@@ -1,20 +1,28 @@
-/* global bootstrap: false */
-(() => {
-    'use strict'
-    const tooltipTriggerList = Array.from(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-    tooltipTriggerList.forEach(tooltipTriggerEl => {
-        new bootstrap.Tooltip(tooltipTriggerEl)
-    })
-})();
-
-
 /*show/hide menu*/
 $('#menu-button').click(function () {
     $('#menu').toggleClass('hide mobile');
 });
 
-JSUtils.querySelectorAll('input[required]', selectedItems =>
-    selectedItems.forEach(e => e ? e.parentElement.classList.add('required-input') : null))
+// JSUtils.querySelectorAll('input[required]', selectedItems =>
+//     selectedItems.forEach(e => e ? e.parentElement.classList.add('required-input') : null))
+document.querySelectorAll('input[required]').forEach(e => {
+    const parentDiv = e.closest('.col');
+    if (parentDiv) {
+        const labelElement = parentDiv.querySelector('label[for="' + e.id + '"]');
+        if (labelElement) {
+            labelElement.classList.add('required-input');
+        }
+    }
+})
+document.querySelectorAll('select[required]').forEach(e => {
+    const parentDiv = e.closest('.col');
+    if (parentDiv) {
+        const labelElement = parentDiv.querySelector('label[for="' + e.id + '"]');
+        if (labelElement) {
+            labelElement.classList.add('required-input');
+        }
+    }
+})
 
 // importação dos arquivos
 
