bd0fd19520e7244f72d1dda26d05a00314eea1ee
Jardel Nunes <jarnunesc@gmail.com>
2024-02-05 18:13:21 -0300

    commit parcial

---
+++ src/main/resources/static/js/autocomplete.js
@@ -0,0 +1,3 @@
+$("#completeBeneficiario").select2(getSettingsSelect2('/api/autocomplete/beneficiarios'));
+$("#completeCesta").select2(getSettingsSelect2('/api/autocomplete/cestas'));
+$("#completeVoluntario").select2(getSettingsSelect2('/api/autocomplete/voluntarios'));
