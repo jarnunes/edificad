a0e16d046d4b7c9d570ed173b51f99fd78e96494
Jardel Nunes <jarnunesc@gmail.com>
2024-05-19 20:42:04 -0300

    Refatorar atributos de configuração do dashboard

--- src/main/java/com/puc/edificad/model/dto/ConfiguracaoDashboardDto.java
+++ src/main/java/com/puc/edificad/model/dto/ConfiguracaoDashboardDto.java
@@ -12,11 +12,11 @@ public class ConfiguracaoDashboardDto implements Serializable {
 
     @Serial
     private static final long serialVersionUID = -4080071506119513659L;
-    private Integer maximoPeriodoEmMesesPermitidosExibicao;
+    private Integer periodoMaximoExibicaoPermitidoEmMeses;
     private Integer periodoExibicaoEmMeses;
 
     public ConfiguracaoDashboardDto() {
-        this.maximoPeriodoEmMesesPermitidosExibicao = 6;
+        this.periodoMaximoExibicaoPermitidoEmMeses = 6;
         this.periodoExibicaoEmMeses = 3;
     }
 }
